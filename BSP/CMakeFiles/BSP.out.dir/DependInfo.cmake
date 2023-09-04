05f343500974deb52181aeffe53056a0c4263846
rohirto <rohirto@gmail.com>
2023-09-04 12:07:17 +0530

    1st commit

---
+++ BSP/CMakeFiles/BSP.out.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/startup_stm32f103xb.s" "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/CMakeFiles/BSP.out.dir/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "Core/Inc"
+  "Drivers/STM32F1xx_HAL_Driver/Inc"
+  "Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Core/Src/main.c" "CMakeFiles/BSP.out.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Core/Src/main.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/BSP.out.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Core/Src/stm32f1xx_it.c" "CMakeFiles/BSP.out.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Core/Src/system_stm32f1xx.c" "CMakeFiles/BSP.out.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  "C:/Users/rohir/OneDrive/Documents/stm32c0/stm32c0/BSP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj" "gcc" "CMakeFiles/BSP.out.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ BSP/CMakeFiles/stm32c0.out.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for stm32c0.out.
+# This may be replaced when dependencies are built.
